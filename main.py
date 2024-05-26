ea3cc0c4c4ba00b20b50a5ea47891a5b1a63abf3
reskenazi <30558584+reskenazi@users.noreply.github.com>
2024-05-26 21:15:12 +0000

    Completed Euler3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import euler1
 import euler2
+import euler3
 
 print("Pick problem number: ")
 problem = input()
@@ -7,3 +8,5 @@ if problem == "1":
   euler1.run()
 elif problem == "2":
   euler2.run()
+elif problem == "3":
+  euler3.run()
